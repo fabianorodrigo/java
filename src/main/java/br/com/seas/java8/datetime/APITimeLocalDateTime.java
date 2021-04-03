efc1c66300d61b78787fca44ee4c18b7c6fead44
Fabiano Nascimento <fabianorodrigo@gmail.com>
2021-04-03 12:29:03 -0300

    Formatação

--- src/main/java/br/com/seas/java8/datetime/APITimeLocalDateTime.java
+++ src/main/java/br/com/seas/java8/datetime/APITimeLocalDateTime.java
@@ -3,11 +3,11 @@ package br.com.seas.java8.datetime;
 import java.time.LocalDateTime;
 
 public class APITimeLocalDateTime {
-    
-        //Data e hora (sem timezone)
-        public static void main( String[] args )
-        {
-            LocalDateTime n = LocalDateTime.now();
-            System.out.println("Data e Hora local agora: " + n.toString() );
-        }
+
+    // Data e hora (sem timezone)
+    public static void main(String[] args) {
+        LocalDateTime n = LocalDateTime.now();
+
+        System.out.println("Data e Hora local agora: " + n.toString());
+    }
 }
--- src/main/java/br/com/seas/java8/datetime/APITimeLocalTime.java
+++ src/main/java/br/com/seas/java8/datetime/APITimeLocalTime.java
@@ -5,14 +5,13 @@ import java.time.temporal.ChronoUnit;
 
 public class APITimeLocalTime {
 
-//Somente hora (sem data, sem timezone)
-        public static void main( String[] args )
-        {
+        // Somente hora (sem data, sem timezone)
+        public static void main(String[] args) {
                 LocalTime n = LocalTime.now();
-                System.out.println("Somente hora agora: " + n.toString() );
-
+                System.out.println("Somente hora agora: " + n.toString());
                 LocalTime lt = n.plus(1, ChronoUnit.HOURS);
-                System.out.println("É possível adicionar horas/minutos/segundos ao um LocalTime com o método imutável plus: " + lt.toString());
-        
+                System.out.println(
+                                "É possível adicionar horas/minutos/segundos ao um LocalTime com o método imutável plus: "
+                                                + lt.toString());
         }
 }
