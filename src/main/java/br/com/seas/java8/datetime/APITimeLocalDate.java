a05d3ceaa6841d55f0e8cc2022f410f8db5d218d
Fabiano Nascimento <fabianorodrigo@gmail.com>
2021-04-03 12:31:53 -0300

    Formatação

--- src/main/java/br/com/seas/java8/datetime/APITimeLocalDate.java
+++ src/main/java/br/com/seas/java8/datetime/APITimeLocalDate.java
@@ -4,17 +4,18 @@ import java.time.LocalDate;
 
 public class APITimeLocalDate {
 
-    //Somente data (sem hora, sem timezone)
-    public static void main( String[] args )
-    {
+    // Somente data (sem hora, sem timezone)
+    public static void main(String[] args) {
         LocalDate n = LocalDate.now();
-        System.out.println("Somente data agora: " + n.toString() );
+        System.out.println("Somente data agora: " + n.toString());
 
-        
-        LocalDate ld = LocalDate.of(1979,7,16);
-        System.out.println("Ao contrário da antiga classe de data, os meses não iniciam em 0 (janeiro) mas sim em 1 (janeiro): " + ld.toString());
+        LocalDate ld = LocalDate.of(1979, 7, 16);
+        System.out.println(
+                "Ao contrário da antiga classe de data, os meses não iniciam em 0 (janeiro) mas sim em 1 (janeiro): "
+                        + ld.toString());
 
         LocalDate ld2 = ld.plusDays(1);
-        System.out.println("É possível adicionar dias ao um LocalDate com o método imutável plusDays: " + ld2.toString());
+        System.out
+                .println("É possível adicionar dias ao um LocalDate com o método imutável plusDays: " + ld2.toString());
     }
 }
--- src/main/java/br/com/seas/java8/datetime/APITimeZonedDateTime.java
+++ src/main/java/br/com/seas/java8/datetime/APITimeZonedDateTime.java
@@ -3,11 +3,10 @@ package br.com.seas.java8.datetime;
 import java.time.ZonedDateTime;
 
 public class APITimeZonedDateTime {
-    
-    //Data, hora e TimeZone
-    public static void main( String[] args )
-    {
+
+    // Data, hora e TimeZone
+    public static void main(String[] args) {
         ZonedDateTime n = ZonedDateTime.now();
-        System.out.println("Data e Hora com o timezone: " + n.toString() );
+        System.out.println("Data e Hora com o timezone: " + n.toString());
     }
 }
